23f9bab6bbccdaafe0553906b0bffd067832dfb5
Michał Szuleta <mszuleta98@gmail.com>
2023-08-07 23:08:07 +0200

    feat : vite config for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key : './client.key',
-      cert: './client.crt',
-    }
-  },
   plugins: [react()],
 })
- 
+  
